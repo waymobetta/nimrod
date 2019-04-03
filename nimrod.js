7cb3357731dbe505d0f2e1c8a04d6f100b40b5b4
waymobetta <jbroethke@gmail.com>
2019-04-03 10:37:31 -0700

    init

---
+++ nimrod.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const keythereum = require('keythereum')
+
+const keystore_file = './keystore.json'
+const pass_file = './passwords'
+
+const keyObj = JSON.parse(fs.readFileSync(keystore_file, 'utf8'))
+const passwords = fs.readFileSync(pass_file, 'utf8')
+
+password_list = passwords.split('\n')
+
+password_list.forEach(password => {
+	try {
+		const private_key = keythereum.recover(password, keyObj)
+		console.log(private_key.toString('hex'))
+		if (private_key.length === 32) {
+			console.log(`[*] found password: ${password}`)
+			process.exit(1)
+		}
+	} catch (err) {
+		console.log(`[-] incorrect password: ${password}`)
+	}
+})
